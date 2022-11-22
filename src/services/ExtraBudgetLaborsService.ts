d36ff49a7eef5f387cc0a3a5a80e7e2af5d37a39
EstebanVU <estebanvargasu@gmail.com>
2022-11-22 10:13:50 -0600

    ExtraBudget Services

---
+++ src/services/ExtraBudgetLaborsService.ts
@@ -0,0 +1,196 @@
+import {
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  runTransaction,
+} from 'firebase/firestore';
+import { FirebaseError } from 'firebase/app';
+import { db } from '../config/firebaseConfig';
+import { IBudgetLabor } from '../types/budgetLabor';
+import { IService } from '../types/service';
+import { toastSuccess, toastError } from '../utils/toast';
+
+export const getExtraBudgetLabors = async ({
+  projectId,
+  appStrings,
+  successCallback,
+  errorCallback,
+}: { projectId: string } & IService) => {
+  try {
+    const userRef = collection(
+      db,
+      'projects',
+      projectId,
+      'projectExtraBudget',
+      'summary',
+      'budgetLabors',
+    );
+    const result = await getDocs(userRef);
+    const data = result.docs.map(doc => ({
+      ...doc.data(),
+      id: doc.id,
+      subtotal: doc.data().cost * doc.data().quantity,
+    })) as IBudgetLabor[];
+
+    successCallback && successCallback(data);
+  } catch (error) {
+    let errorMessage = appStrings.genericError;
+    if (error instanceof FirebaseError) errorMessage = error.message;
+
+    toastError(appStrings.getInformationError, errorMessage);
+
+    errorCallback && errorCallback();
+  }
+};
+
+export const getExtraBudgetLaborById = async ({
+  projectId,
+  extraBudgetLaborId,
+  appStrings,
+  successCallback,
+  errorCallback,
+}: {
+  projectId: string;
+  extraBudgetLaborId: string;
+} & IService) => {
+  try {
+    const userRef = doc(
+      db,
+      'projects',
+      projectId,
+      'projectExtraBudget',
+      'summary',
+      'budgetLabors',
+      extraBudgetLaborId,
+    );
+    const result = await getDoc(userRef);
+    const data = {
+      ...result.data(),
+      id: result.id,
+      subtotal: result.data()?.cost * result.data()?.quantity,
+    } as IBudgetLabor;
+
+    successCallback && successCallback(data);
+  } catch (error) {
+    let errorMessage = appStrings.genericError;
+    if (error instanceof FirebaseError) errorMessage = error.message;
+
+    toastError(appStrings.getInformationError, errorMessage);
+
+    errorCallback && errorCallback();
+  }
+};
+
+export const createExtraBudgetLabor = async ({
+  projectId,
+  extraBudgetLabor,
+  appStrings,
+  successCallback,
+  errorCallback,
+}: {
+  projectId: string;
+  extraBudgetLabor: IBudgetLabor;
+} & IService) => {
+  try {
+    const data = await runTransaction(db, async transaction => {
+      const { id, subtotal, ...rest } = extraBudgetLabor;
+      const laborRef = doc(
+        collection(
+          db,
+          'projects',
+          projectId,
+          'projectExtraBudget',
+          'summary',
+          'budgetLabors',
+        ),
+      );
+      const sumRef = doc(
+        db,
+        'projects',
+        projectId,
+        'projectExtraBudget',
+        'summary',
+      );
+      const sumDoc = await transaction.get(sumRef);
+
+      if (!sumDoc.exists()) throw Error(appStrings.noRecords);
+
+      const total = sumDoc.data().sumLabors + subtotal;
+
+      transaction.update(sumRef, { sumLabors: total });
+      transaction.set(laborRef, rest);
+
+      return {
+        ...extraBudgetLabor,
+        id: laborRef.id,
+      } as IBudgetLabor;
+    });
+
+    toastSuccess(appStrings.success, appStrings.saveSuccess);
+
+    successCallback && successCallback(data);
+  } catch (error) {
+    let errorMessage = appStrings.genericError;
+    if (error instanceof FirebaseError) errorMessage = error.message;
+
+    toastError(appStrings.saveError, errorMessage);
+
+    errorCallback && errorCallback();
+  }
+};
+
+export const updateExtraBudgetLabor = async ({
+  projectId,
+  extraBudgetLabor,
+  appStrings,
+  successCallback,
+  errorCallback,
+}: {
+  projectId: string;
+  extraBudgetLabor: IBudgetLabor;
+} & IService) => {
+  try {
+    await runTransaction(db, async transaction => {
+      const { id, subtotal, ...rest } = extraBudgetLabor;
+      const laborRef = doc(
+        db,
+        'projects',
+        projectId,
+        'projectExtraBudget',
+        'summary',
+        'budgetLabors',
+        id,
+      );
+      const sumRef = doc(
+        db,
+        'projects',
+        projectId,
+        'projectExtraBudget',
+        'summary',
+      );
+      const laborDoc = await transaction.get(laborRef);
+      const sumDoc = await transaction.get(sumRef);
+
+      if (!laborDoc.exists() || !sumDoc.exists())
+        throw Error(appStrings.noRecords);
+
+      const newSum = subtotal - laborDoc.data().cost * laborDoc.data().quantity;
+      const total = sumDoc.data().sumLabors + newSum;
+
+      transaction.update(sumRef, { sumLabors: total });
+      transaction.set(laborRef, rest);
+    });
+
+    toastSuccess(appStrings.success, appStrings.saveSuccess);
+
+    successCallback && successCallback();
+  } catch (error) {
+    let errorMessage = appStrings.genericError;
+    if (error instanceof FirebaseError) errorMessage = error.message;
+
+    toastError(appStrings.saveError, errorMessage);
+
+    errorCallback && errorCallback();
+  }
+};
---
+++ src/services/ProjectExtraBudgetService.ts
@@ -0,0 +1,67 @@
+import { getDoc, doc, updateDoc } from 'firebase/firestore';
+import { FirebaseError } from 'firebase/app';
+import { db } from '../config/firebaseConfig';
+import { IProjectBudget } from '../types/projectBudget';
+import { IService } from '../types/service';
+import { toastSuccess, toastError } from '../utils/toast';
+
+export const getProjectExtraBudget = async ({
+  projectId,
+  appStrings,
+  successCallback,
+  errorCallback,
+}: { projectId: string } & IService) => {
+  try {
+    const userRef = doc(
+      db,
+      'projects',
+      projectId,
+      'projectExtraBudget',
+      'summary',
+    );
+    const result = await getDoc(userRef);
+    const data = result.data() as IProjectBudget;
+
+    successCallback && successCallback(data);
+  } catch (error) {
+    let errorMessage = appStrings.genericError;
+    if (error instanceof FirebaseError) errorMessage = error.message;
+
+    toastError(appStrings.getInformationError, errorMessage);
+
+    errorCallback && errorCallback();
+  }
+};
+
+export const updateProjectExtraBudgetExchange = async ({
+  projectId,
+  exchange,
+  appStrings,
+  successCallback,
+  errorCallback,
+}: {
+  projectId: string;
+  exchange: number;
+} & IService) => {
+  try {
+    const extraBudgetRef = doc(
+      db,
+      'projects',
+      projectId,
+      'projectExtraBudget',
+      'summary',
+    );
+    await updateDoc(extraBudgetRef, { exchange });
+
+    toastSuccess(appStrings.success, appStrings.saveSuccess);
+
+    successCallback && successCallback();
+  } catch (error) {
+    let errorMessage = appStrings.genericError;
+    if (error instanceof FirebaseError) errorMessage = error.message;
+
+    toastError(appStrings.saveError, errorMessage);
+
+    errorCallback && errorCallback();
+  }
+};
