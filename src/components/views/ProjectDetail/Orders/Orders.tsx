405e62ea653f34d6e9cc3a0b5b2b9e1445700f91
Hernán Alfaro Araya <55323501+KuroScar42@users.noreply.github.com>
2023-02-14 12:35:00 -0600

    Applying feedback, the materials no longer filled with the budget materials

--- src/components/views/ProjectDetail/Orders/Orders.tsx
+++ src/components/views/ProjectDetail/Orders/Orders.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import * as yup from 'yup';
 import { useNavigate } from 'react-router-dom';
@@ -64,7 +64,7 @@ const initialSelectedOrderData = {
 const initialSelectedProductData = {
   id: '',
   quantity: 1,
-  description: { value: '', label: '' },
+  description: '',
   cost: 0,
   materialRef: '',
 };
@@ -79,14 +79,10 @@ const initialMaterialRefData = {
 const Orders: React.FC<IOrdersView> = props => {
   const { projectId } = props;
   const [tableData, setTableData] = useState<IProjectOrder[]>([]);
-  const [materials, setMaterials] = useState<IMaterialBreakdown[]>([]);
-  const [extraMaterials, setExtraMaterials] = useState<IMaterialBreakdown[]>(
-    [],
-  );
   const [selectedOrder, setSelectedOrder] = useState<IOrder>(
     initialSelectedOrderData,
   );
-  const [selectedProduct, setSelectedProduct] = useState<IProduct>(
+  const [selectedProduct, setSelectedProduct] = useState<IOrderProduct>(
     initialSelectedProductData,
   );
   const [selectedTab, setSelectedTab] = useState('draft');
@@ -162,26 +158,6 @@ const Orders: React.FC<IOrdersView> = props => {
     await getProjectActivities({ projectId, appStrings, successCallback });
   };
 
-  const getMaterials = async () => {
-    const successCallback = (response: IMaterialBreakdown[]) =>
-      setMaterials(response);
-    await getBudgetMaterials({
-      projectId,
-      appStrings,
-      successCallback,
-    });
-  };
-
-  const getExtraMaterials = async () => {
-    const successCallback = (response: IMaterialBreakdown[]) =>
-      setExtraMaterials(response);
-    await getExtraBudgetMaterials({
-      projectId,
-      appStrings,
-      successCallback,
-    });
-  };
-
   const addItem = (item: IProjectOrder) => setTableData([item, ...tableData]);
 
   const updateItem = (item: IProjectOrder) => {
@@ -200,55 +176,6 @@ const Orders: React.FC<IOrdersView> = props => {
 
   const handleSearch = async (event: { target: { value: string } }) => {
     setSearchTerm(event.target.value.toUpperCase());
-  };
-
-  const handleSearchSelect = async (id: string) => {
-    let material, matRef;
-    matRef = materials.find(mat =>
-      id === mat.id
-        ? (material = mat?.material)
-        : (material = mat?.subMaterials.find(subMat => id === subMat.id)!),
-    );
-
-    if (material) {
-      const { id, quantity, cost, name } = material;
-      setSelectedProduct({
-        ...selectedProduct,
-        quantity,
-        cost,
-        description: { value: id, label: name },
-      });
-      setMaterialRef({
-        materialId: matRef?.id!,
-        subMaterialId: id,
-        isExtraMaterial: false,
-        isSubMaterial: matRef?.material?.hasSubMaterials!,
-      });
-    } else {
-      let extraMaterial, extraMatRef;
-      extraMatRef = extraMaterials.find(mat =>
-        id === mat.id
-          ? (extraMaterial = mat?.material)
-          : (extraMaterial = mat?.subMaterials.find(
-              subMat => id === subMat.id,
-            )!),
-      );
-      if (extraMaterial) {
-        const { id, quantity, cost, name } = extraMaterial;
-        setSelectedProduct({
-          ...selectedProduct,
-          quantity,
-          cost,
-          description: { value: id, label: name },
-        });
-        setMaterialRef({
-          materialId: extraMatRef?.id!,
-          subMaterialId: id,
-          isExtraMaterial: true,
-          isSubMaterial: extraMatRef?.material?.hasSubMaterials!,
-        });
-      }
-    }
   };
 
   const handleOnChangeActivity = (e: any) => {
@@ -313,10 +240,7 @@ const Orders: React.FC<IOrdersView> = props => {
         activity: getFormattedActivity(order?.activity),
       };
       setSelectedOrder(parsedOrder);
-      setSelectedProduct({
-        ...product,
-        description: { value: product.id, label: product.description },
-      });
+      setSelectedProduct(product);
       setIsProductModalOpen(true);
     }
   };
@@ -374,11 +298,11 @@ const Orders: React.FC<IOrdersView> = props => {
       : await createProjectOrder(serviceCallParameters);
   };
 
-  const onSubmitProduct = async (data: IProduct) => {
+  const onSubmitProduct = async (data: IOrderProduct) => {
     const { description, ...rest } = data;
     const product = {
       ...rest,
-      description: description.label,
+      description,
       quantity: +rest.quantity,
       cost: +rest.cost,
     };
@@ -437,48 +361,10 @@ const Orders: React.FC<IOrdersView> = props => {
   });
 
   const productValSchema = yup.object().shape({
-    description: yup.object().shape({
-      value: yup.string().required(appStrings?.requiredField),
-      label: yup.string().required(appStrings?.requiredField),
-    }),
+    description: yup.string().required(appStrings?.requiredField),
     quantity: yup.string().required(appStrings?.requiredField),
     cost: yup.string().required(appStrings?.requiredField),
   });
-
-  const options = () => {
-    let optionsList: { value: string; label: string }[] = [];
-    materials.forEach(material => {
-      if (!material?.material?.hasSubMaterials) {
-        optionsList.push({
-          value: material.id,
-          label: material?.material?.name,
-        });
-      } else {
-        material?.subMaterials.forEach(subMaterial =>
-          optionsList.push({
-            value: subMaterial.id,
-            label: subMaterial?.name,
-          }),
-        );
-      }
-    });
-    extraMaterials.forEach(material => {
-      if (!material?.material?.hasSubMaterials) {
-        optionsList.push({
-          value: material.id,
-          label: material?.material?.name,
-        });
-      } else {
-        material?.subMaterials.forEach(subMaterial =>
-          optionsList.push({
-            value: subMaterial.id,
-            label: subMaterial?.name,
-          }),
-        );
-      }
-    });
-    return optionsList;
-  };
 
   const formatActOptions = () => {
     const isActivityUsed = (activityId: string) =>
@@ -494,194 +380,193 @@ const Orders: React.FC<IOrdersView> = props => {
   useEffect(() => {
     let abortController = new AbortController();
     getOrders();
-    getMaterials();
-    getExtraMaterials();
     getActivities();
     return () => abortController.abort();
   }, []);
 
   return (
     <div className={`${styles.operations_container}`}>
-      <Flex marginBottom="5px">
-        <SearchInput
-          style={{ margin: '0 10px 0 0', maxWidth: '500px' }}
-          placeholder="Search"
-          onChange={handleSearch}
-        ></SearchInput>
-        <div style={{ textAlign: 'end' }}>
-          <Button
-            onClick={() => {
-              tableData.length
-                ? setSelectedOrder({
-                    ...initialSelectedOrderData,
-                    order: tableData[0].order + 1,
-                  })
-                : setSelectedOrder(initialSelectedOrderData);
-              setIsModalOpen(true);
-            }}
-          >
-            +
-          </Button>
-          <Modal
-            isOpen={isModalOpen}
-            onClose={() => {
-              setSelectedOrder(initialSelectedOrderData);
-              setIsModalOpen(false);
-            }}
-          >
-            <Heading as="h2" size="lg">
-              {selectedOrder.id ? appStrings.editOrder : appStrings.addOrder}
-            </Heading>
-            <Form
-              id="order-form"
-              initialFormData={selectedOrder}
-              validationSchema={validationSchema}
-              validateOnChange
-              validateOnBlur
-              onSubmit={handleOnSubmit}
-            >
-              <Input
-                name="order"
-                type="number"
-                label={appStrings.order}
-                isDisabled
-              />
-              <SearchSelect
-                name="activity"
-                label={appStrings.activity}
-                placeholder={appStrings.activity}
-                isDisabled={!!selectedOrder.id}
-                options={formatActOptions()}
-                value={selectedOrder.activity}
-                onChange={item => handleOnChangeActivity(item)}
-              />
-              <Input
-                name="proforma"
-                type="number"
-                label={appStrings.proforma}
-              />
-              <DatePicker name="date" label={appStrings.date}></DatePicker>
-              <DatePicker
-                name="deliverDate"
-                label={appStrings.deliverDate}
-              ></DatePicker>
-              <Select
-                name="sentStatus"
-                options={orderStatus}
-                label={appStrings.status}
-              />
-              <br />
-              <Button width="full" type="submit">
-                {appStrings.submit}
-              </Button>
-            </Form>
-          </Modal>
-          <Modal
-            isOpen={isProductModalOpen}
-            onClose={() => {
-              setSelectedOrder(initialSelectedOrderData);
-              setSelectedProduct(initialSelectedProductData);
-              setIsProductModalOpen(false);
-            }}
-          >
-            <Heading as="h2" size="lg">
-              {selectedProduct.id
-                ? appStrings.editProduct
-                : appStrings.addProduct}
-            </Heading>
-            <Form
-              id="product-form"
-              initialFormData={selectedProduct}
-              validationSchema={productValSchema}
-              validateOnChange
-              validateOnBlur
-              onSubmit={onSubmitProduct}
-            >
-              <SearchSelect
-                name="description"
-                label={appStrings.material}
-                placeholder={appStrings.material}
-                isDisabled={!!selectedProduct.id}
-                options={options()}
-                value={selectedProduct.description}
-                onChange={item => handleSearchSelect(item?.value?.value)}
-              />
-              <Input
-                name="quantity"
-                label={appStrings.quantity}
-                innerStyle={{ width: '200px', marginRight: '5px' }}
-              />
-              <Input
-                name="cost"
-                type={'number'}
-                label={appStrings.cost}
-                innerStyle={{ width: '200px', marginRight: '5px' }}
-              />
-              <br />
-              <Button width="full" type="submit">
-                {appStrings.submit}
-              </Button>
-            </Form>
-          </Modal>
+      <Box p={5} borderWidth="1px" borderRadius={12}>
+        <div className={styles.tabsContainer}>
+          <TabGroup
+            className={styles.tabs}
+            tabs={[
+              { id: 'pending', name: appStrings.pending, selected: true },
+              { id: 'sent', name: appStrings.sent },
+            ]}
+            variant="rounded"
+            onSelectedTabChange={activeTabs => setSelectedTab(activeTabs[0])}
+          />
         </div>
-      </Flex>
-      <AlertDialog
-        title={appStrings.deleteOrder}
-        content={appStrings.deleteWarning}
-        isOpen={isAlertDialogOpen}
-        onClose={() => {
-          setSelectedOrder(initialSelectedOrderData);
-          setIsAlertDialogOpen(false);
-        }}
-        onSubmit={() => deleteButton()}
-      />
-      <AlertDialog
-        title={appStrings.deleteProduct}
-        content={appStrings.deleteWarning}
-        isOpen={isProductAlertDialogOpen}
-        onClose={() => {
-          setSelectedProduct(initialSelectedProductData);
-          setMaterialRef(initialMaterialRefData);
-          setIsProductAlertDialogOpen(false);
-        }}
-        onSubmit={() => deleteProduct()}
-      />
-      <div className={styles.tabsContainer}>
-        <TabGroup
-          className={styles.tabs}
-          tabs={[
-            { id: 'pending', name: appStrings.pending, selected: true },
-            { id: 'sent', name: appStrings.sent },
-          ]}
-          variant="rounded"
-          onSelectedTabChange={activeTabs => setSelectedTab(activeTabs[0])}
+        <Flex marginBottom="5px" className={styles.p_10}>
+          <SearchInput
+            style={{ margin: '0 10px 0 0', maxWidth: '500px' }}
+            placeholder="Search"
+            onChange={handleSearch}
+          ></SearchInput>
+          <div style={{ textAlign: 'end' }}>
+            <Button
+              onClick={() => {
+                tableData.length
+                  ? setSelectedOrder({
+                      ...initialSelectedOrderData,
+                      order: tableData[0].order + 1,
+                    })
+                  : setSelectedOrder(initialSelectedOrderData);
+                setIsModalOpen(true);
+              }}
+            >
+              +
+            </Button>
+            <Modal
+              isOpen={isModalOpen}
+              onClose={() => {
+                setSelectedOrder(initialSelectedOrderData);
+                setIsModalOpen(false);
+              }}
+            >
+              <Heading as="h2" size="lg">
+                {selectedOrder.id ? appStrings.editOrder : appStrings.addOrder}
+              </Heading>
+              <Form
+                id="order-form"
+                initialFormData={selectedOrder}
+                validationSchema={validationSchema}
+                validateOnChange
+                validateOnBlur
+                onSubmit={handleOnSubmit}
+              >
+                <Input
+                  name="order"
+                  type="number"
+                  label={appStrings.order}
+                  isDisabled
+                />
+                <SearchSelect
+                  name="activity"
+                  label={appStrings.activity}
+                  placeholder={appStrings.activity}
+                  isDisabled={!!selectedOrder.id}
+                  options={formatActOptions()}
+                  value={selectedOrder.activity}
+                  onChange={item => handleOnChangeActivity(item)}
+                />
+                <Input
+                  name="proforma"
+                  type="number"
+                  label={appStrings.proforma}
+                />
+                <DatePicker
+                  name="date"
+                  label={appStrings.creationDate}
+                ></DatePicker>
+                <DatePicker
+                  name="deliverDate"
+                  label={appStrings.deliverDate}
+                ></DatePicker>
+                <Select
+                  name="sentStatus"
+                  options={orderStatus}
+                  label={appStrings.status}
+                />
+                <br />
+                <Button width="full" type="submit">
+                  {appStrings.submit}
+                </Button>
+              </Form>
+            </Modal>
+            <Modal
+              isOpen={isProductModalOpen}
+              onClose={() => {
+                setSelectedOrder(initialSelectedOrderData);
+                setSelectedProduct(initialSelectedProductData);
+                setIsProductModalOpen(false);
+              }}
+            >
+              <Heading as="h2" size="lg">
+                {selectedProduct.id
+                  ? appStrings.editProduct
+                  : appStrings.addProduct}
+              </Heading>
+              <Form
+                id="product-form"
+                initialFormData={selectedProduct}
+                validationSchema={productValSchema}
+                validateOnChange
+                validateOnBlur
+                onSubmit={onSubmitProduct}
+              >
+                <Input
+                  name="description"
+                  label={appStrings.material}
+                  isDisabled={!!selectedProduct.id}
+                />
+                <Input
+                  name="quantity"
+                  label={appStrings.quantity}
+                  innerStyle={{ width: '200px', marginRight: '5px' }}
+                />
+                <Input
+                  name="cost"
+                  type={'number'}
+                  label={appStrings.cost}
+                  innerStyle={{ width: '200px', marginRight: '5px' }}
+                />
+                <br />
+                <Button width="full" type="submit">
+                  {appStrings.submit}
+                </Button>
+              </Form>
+            </Modal>
+          </div>
+        </Flex>
+        <AlertDialog
+          title={appStrings.deleteOrder}
+          content={appStrings.deleteWarning}
+          isOpen={isAlertDialogOpen}
+          onClose={() => {
+            setSelectedOrder(initialSelectedOrderData);
+            setIsAlertDialogOpen(false);
+          }}
+          onSubmit={() => deleteButton()}
         />
-      </div>
+        <AlertDialog
+          title={appStrings.deleteProduct}
+          content={appStrings.deleteWarning}
+          isOpen={isProductAlertDialogOpen}
+          onClose={() => {
+            setSelectedProduct(initialSelectedProductData);
+            setMaterialRef(initialMaterialRefData);
+            setIsProductAlertDialogOpen(false);
+          }}
+          onSubmit={() => deleteProduct()}
+        />
 
-      <OrdersTableView
-        headers={tableHeader}
-        items={formatTableData()}
-        filter={value =>
-          searchTerm === '' || value?.order?.toString()?.includes(searchTerm)
-        }
-        handleRowClick={() => {}}
-        onClickEdit={id => editButton(id)}
-        onClickDelete={id => {
-          setSelectedOrder({ ...selectedOrder, id });
-          setIsAlertDialogOpen(true);
-        }}
-        onClickExportPDF={id => exportPDFButton(id)}
-        onClickAddProduct={id => addProduct(id)}
-        onClickEditProduct={(orderId, productId) =>
-          editProduct(orderId, productId)
-        }
-        onClickDeleteProduct={(orderId, productId) =>
-          delProduct(orderId, productId)
-        }
-        exchangeRate={Number('0.13')} //Modify with the exchange of the project when is done
-        formatCurrency
-      />
-      {!tableData.length ? <h1>{appStrings.noRecords}</h1> : null}
+        <OrdersTableView
+          headers={tableHeader}
+          items={formatTableData()}
+          filter={value =>
+            searchTerm === '' || value?.order?.toString()?.includes(searchTerm)
+          }
+          handleRowClick={() => {}}
+          onClickEdit={id => editButton(id)}
+          onClickDelete={id => {
+            setSelectedOrder({ ...selectedOrder, id });
+            setIsAlertDialogOpen(true);
+          }}
+          onClickExportPDF={id => exportPDFButton(id)}
+          onClickAddProduct={id => addProduct(id)}
+          onClickEditProduct={(orderId, productId) =>
+            editProduct(orderId, productId)
+          }
+          onClickDeleteProduct={(orderId, productId) =>
+            delProduct(orderId, productId)
+          }
+          exchangeRate={Number('0.13')} //Modify with the exchange of the project when is done
+          formatCurrency
+        />
+        {!tableData.length ? <h1>{appStrings.noRecords}</h1> : null}
+      </Box>
     </div>
   );
 };
